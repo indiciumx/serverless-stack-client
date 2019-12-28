dcd1098b58eeea6f562c841cc57b52e575300fbf
indiciumx <chris@indiciumx.com>
2019-12-27 22:04:47 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "US-EAST-1",
+      BUCKET: "notes-app-uploads-tutorial-1912"
+    },
+    apiGateway: {
+      REGION: "US-EAST-1",
+      URL: "https://28xi2dpvth.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "US-EAST-1",
+      USER_POOL_ID: "us-east-1_caNDhH12l",
+      APP_CLIENT_ID: "1g4mf0hshkhtpdtpjheiocvjtb",
+      IDENTITY_POOL_ID: "us-east-1_caNDhH12l"
+    }
+  };
